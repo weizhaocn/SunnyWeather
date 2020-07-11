a468b421abdb4666d630882303a2a19d66a6c5ec
zhaowei <zhaowei@unioncast.cn>
2020-07-11 13:42:32 +0800

    新增切换城市和手动更新天气的功能

--- app/src/main/java/com/john/sunnyweather/ui/place/PlaceAdapter.kt
+++ app/src/main/java/com/john/sunnyweather/ui/place/PlaceAdapter.kt
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.john.sunnyweather.R
 import com.john.sunnyweather.logic.model.Place
 import com.john.sunnyweather.ui.weather.WeatherActivity
+import kotlinx.android.synthetic.main.activity_weather.*
 import kotlinx.android.synthetic.main.place_item.view.*
 
 /**
@@ -28,14 +29,24 @@ class PlaceAdapter(val fragment: PlaceFragment, val places: List<Place>): Recycl
         holder.itemView.setOnClickListener {
             val position = holder.adapterPosition
             val place = places[position]
-            val intent = Intent(parent.context, WeatherActivity::class.java).apply {
-                putExtra("location_lng", place.location.lng)
-                putExtra("location_lat", place.location.lat)
-                putExtra("place_name", place.name)
+            val activity = fragment.activity
+            if(activity is WeatherActivity){
+                activity.drawerLayout.closeDrawers()
+                activity.viewModel.locationLng = place.location.lng
+                activity.viewModel.locationLat = place.location.lat
+                activity.viewModel.placeName = place.name
+                activity.refreshWeather()
+            }else{
+                val intent = Intent(parent.context, WeatherActivity::class.java).apply {
+                    putExtra("location_lng", place.location.lng)
+                    putExtra("location_lat", place.location.lat)
+                    putExtra("place_name", place.name)
+                }
+
+                fragment.startActivity(intent)
+                fragment.activity?.finish()
             }
             fragment.viewModel.savePlace(place)
-            fragment.startActivity(intent)
-            fragment.activity?.finish()
         }
         return holder
     }
--- app/src/main/java/com/john/sunnyweather/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/john/sunnyweather/ui/weather/WeatherActivity.kt
@@ -1,13 +1,17 @@
 package com.john.sunnyweather.ui.weather
 
+import android.content.Context
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.john.sunnyweather.R
@@ -53,8 +57,36 @@ class WeatherActivity : AppCompatActivity() {
                 Toast.makeText(this, "无法成功获取天气信息", Toast.LENGTH_SHORT).show()
                 result.exceptionOrNull() ?. printStackTrace()
             }
+            srlRefresh.isRefreshing = false
         })
+
+        srlRefresh.setColorSchemeResources(R.color.colorPrimary)
+        refreshWeather()
+        srlRefresh.setOnRefreshListener {
+            refreshWeather()
+        }
+
+        btNav.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+        drawerLayout.addDrawerListener(object : DrawerLayout.DrawerListener{
+            override fun onDrawerStateChanged(newState: Int) {}
+
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {}
+
+            override fun onDrawerClosed(drawerView: View) {
+                val manager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                manager.hideSoftInputFromWindow(drawerView.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+            }
+
+            override fun onDrawerOpened(drawerView: View) {}
+
+        })
+    }
+
+    fun refreshWeather(){
         viewModel.refreshWeather(viewModel.locationLng, viewModel.locationLat)
+        srlRefresh.isRefreshing = true
     }
 
     private fun showWeatherInfo(weather: Weather) {
